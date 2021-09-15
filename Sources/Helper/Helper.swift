ce6015720ca7c4ab1925ac683dc836722ffc3b26
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2021-09-15 10:28:43 +0300

    Initial commit

---
+++ Sources/Helper/Helper.swift
@@ -0,0 +1,7 @@
+
+public class Helper {
+    
+    public func summ(_ p1: Int, _ p2: Int) -> Int {
+        return p1 + p2
+    }
+}
---
+++ Tests/HelperTests/HelperTests.swift
@@ -0,0 +1,22 @@
+    import XCTest
+    @testable import Helper
+
+    final class HelperTests: XCTestCase {
+        
+        private let helper = Helper()
+        
+        func testSummA() {
+            let result = helper.summ(2, 2)
+            XCTAssertEqual(result, 4)
+        }
+        
+        func testSummB() {
+            let result = helper.summ(3, 2)
+            XCTAssertEqual(result, 5)
+        }
+        
+        func testSummC() {
+            let result = helper.summ(10, 2)
+            XCTAssertNotEqual(result, 0)
+        }
+    }
